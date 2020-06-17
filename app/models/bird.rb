acf38321098b8da6b0bfe354efe2770df8ad2c25
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-17 14:59:11 +0000

    Automatically backed up by Learn

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,4 +1,11 @@
 class Location < ApplicationRecord
   has_many :sightings
   has_many :birds, through: :sightings
+  def show
+  sighting = Sighting.find_by(id: params[:id])
+  render json: sighting.to_json(:include => {
+    :bird => {:only => [:name, :species]},
+    :location => {:only => [:latitude, :longitude]}
+  }, :except => [:updated_at])
+end
 end
